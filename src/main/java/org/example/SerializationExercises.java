aab14853fe689a651a3d7db557703205c648ada3
Guedes <albertoguedes4@gmail.com>
2023-10-13 09:15:01 +0100

    Second commit

--- src/main/java/org/example/SerializationExercises.java
+++ src/main/java/org/example/SerializationExercises.java
@@ -5,10 +5,7 @@ import org.example.model.Movie;
 import org.example.model.Session;
 import org.example.model.Theatre;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class SerializationExercises {
 	/*
@@ -28,6 +25,8 @@ public class SerializationExercises {
 
 			Session session1 = new Session(movie1, theatre1, "10/07/21");
 			Session session2 = new Session(movie2, theatre2, "15/12/23");
+
+			//Serialization with Json
 
 			Gson gson = new Gson();
 			String movie1Json = gson.toJson(movie1);
@@ -76,6 +75,7 @@ public class SerializationExercises {
 	public static class Exercise2 {
 
 		public static void main(String[] args) throws FileNotFoundException {
+			//Deserialization from Json
 			Gson gson = new Gson();
 			Movie movie1 = gson.fromJson(new FileReader("movie1.json"), Movie.class);
 			Movie movie2 = gson.fromJson(new FileReader("movie2.json"), Movie.class);
@@ -83,6 +83,25 @@ public class SerializationExercises {
 			Theatre theatre2 = gson.fromJson(new FileReader("theater2.json"), Theatre.class);
 			Session session1 = gson.fromJson(new FileReader("session1.json"), Session.class);
 			Session session2 = gson.fromJson(new FileReader("session2.json"), Session.class);
+
+			//Serialization with ObjectOutputStream
+			try (ObjectOutputStream movie1Stream = new ObjectOutputStream(new FileOutputStream("movie1.ser"));
+				 ObjectOutputStream movie2Stream = new ObjectOutputStream(new FileOutputStream("movie2.ser"));
+				 ObjectOutputStream theater1Stream = new ObjectOutputStream(new FileOutputStream("theater1.ser"));
+				 ObjectOutputStream theater2Stream = new ObjectOutputStream(new FileOutputStream("theater2.ser"));
+				 ObjectOutputStream session1Stream = new ObjectOutputStream(new FileOutputStream("session1.ser"));
+				 ObjectOutputStream session2Stream = new ObjectOutputStream(new FileOutputStream("session2.ser")))
+			{
+
+				movie1Stream.writeObject(movie1);
+				movie2Stream.writeObject(movie2);
+				theater1Stream.writeObject(theatre1);
+				theater2Stream.writeObject(theatre2);
+				session1Stream.writeObject(session1);
+				session2Stream.writeObject(session2);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 
